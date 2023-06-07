cc400462e6611b9a2ba3c9bda41aaec8504dbc17
Jordan Masone <jordan.masone@gfs.com>
2023-06-07 12:41:08 -0400

    Initial file creation and testing

---
+++ docs/assets/js/script.js
@@ -0,0 +1,48 @@
+"use strict";
+const icon = document.getElementById("test");
+const wrapper = document.getElementById("wrapper");
+let leftPosition = 0;
+let topPosition = 0;
+let xDirection = "left";
+let yDirection = "top";
+const updatePosition = () => {
+    if (xDirection === "left") {
+        if (leftPosition + 1 > wrapper.offsetWidth) {
+            xDirection = "right";
+            leftPosition -= 2;
+        }
+        else {
+            leftPosition += 2;
+        }
+    }
+    else {
+        if (leftPosition - 1 < 0) {
+            xDirection = "left";
+            leftPosition += 2;
+        }
+        else {
+            leftPosition -= 2;
+        }
+    }
+    if (yDirection === "top") {
+        if (topPosition + 1 > wrapper.clientHeight) {
+            yDirection = "bottom";
+            topPosition -= 2;
+        }
+        else {
+            topPosition += 2;
+        }
+    }
+    else {
+        if (topPosition - 1 < 0) {
+            yDirection = "top";
+            topPosition += 2;
+        }
+        else {
+            topPosition -= 2;
+        }
+    }
+    icon.setAttribute("style", `left: ${leftPosition}px; top: ${topPosition}px;`);
+};
+setInterval(updatePosition, 10);
+console.log(wrapper.clientWidth, wrapper.offsetWidth, wrapper.scrollWidth);
